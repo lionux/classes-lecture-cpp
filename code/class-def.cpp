3da277cf300f7639db548271462164234fe9aa76
Samuel Micka <mickas37@gmail.com>
2020-01-21 20:00:45 -0700

    Add class def cpp

--- code/class-def.cpp
+++ code/class-def.cpp
@@ -1,19 +1,24 @@
 #include <iostream>
 using namespace std;
-
-class ObjectName
+class Course
 {
 public:
   int x;
 private:
   int y;
 };
-
 int main()
 {
-  ObjectName obj;
-  obj.x = 3;
-  cout << "x: " << obj.x << endl;
-  obj.y = 4;
+  Course some_course;
+  some_course.x = 3;
+  some_course.y = 4;
   return 0;
 }
+
+
+
+
+
+//  some_course.x = 3;
+//  cout << "x: " << obj.x << endl;
+//  obj.y = 4;
